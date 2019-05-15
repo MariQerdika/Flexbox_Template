ccc11a176943120c3daadc348c56cbcb8ce650dd
MariQerdika <40671485+MariQerdika@users.noreply.github.com>
2019-05-15 17:18:22 +0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,29 @@ else if ($("nav").hasClass('secondary')){
    $("nav").removeClass('secondary');
 }
 });
+
+
+
+
+
+$('.work').magnificPopup({
+   delegate: 'a', // child items selector, by clicking on it popup will open
+   type: 'image',
+   gallery: {
+      enabled: true
+   }
+});
+
+
+
+var pulse = function () {
+   var e = document.createElement("div");
+   e.setAttribute('class', 'circle'),
+      document.body.appendChild(e),
+      e.style.top = event.pageY + 'px',
+      e.style.left = event.pageX + 'px',
+      setTimeout(function () {
+         document.body.removeChild(e)
+      }, 5000)
+};
+document.addEventListener('click', pulse);
